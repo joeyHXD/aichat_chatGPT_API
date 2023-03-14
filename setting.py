cf3023cbc1467a3013df0f71df509e0e7c25a269
joeyHXD <68325229+joeyHXD@users.noreply.github.com>
2023-03-14 09:30:34 +0800

    Update setting.py
    update comments

--- setting.py
+++ setting.py
@@ -2,19 +2,19 @@ import os
 import json
 
 settings = {
-    "config_file_name": "config.json",#AI概率文件名
-    "group_conversation_file_name": "group_conversation.json",#群AI文件名
-    "temp_chat_file_name": "temp_chat.json",#临时会话文件名
-    "api_key": "",#openai 的 API key, https://platform.openai.com/account/api-keys
-    "prefix": ["/t"], #触发群AI的前缀
+    "config_file_name": "config.json",# AI概率文件名
+    "group_conversation_file_name": "group_conversation.json",# 群AI文件名
+    "temp_chat_file_name": "temp_chat.json",# 临时会话文件名
+    "api_key": "",# openai 的 API key, https://platform.openai.com/account/api-keys
+    "prefix": ["/t"], # 触发群AI的前缀
     "group_context_max": 3, # 设置为-1则无限记录群聊，0则不记录，3则记录触发群AI前的最后3条消息，让群AI更加合群
-    "ai_chat_max_token": 200, #单条群AI回复内容的最大token数，大约50汉字的感觉
-    "temp_chat_max_token": 500, #单条临时会话回复内容的最大token数，注意单次请求必须小于4096token，这包括回复内容和所有的聊天信息，如果超出可能openAPI会报错，我不确定
+    "ai_chat_max_token": 200, # 单条群AI回复内容的最大token数，大约100汉字的感觉
+    "temp_chat_max_token": 500, # 单条临时会话回复内容的最大token数，注意单次请求必须小于4096token，这包括回复内容和所有的聊天信息，如果超出openAPI会报错
     "proxy": None, # 代理
     "model_used": "", # 无效参数，暂时默认gpt-3.5-turbo，以后有空再补上
-    "sleep_time": 60, #默认60秒，临时会话自动结束
-    "DEFAULT_AI_CHANCE": 0, #群AI的默认概率
-    "Keywords": ['bot','BOT','Bot','机器人','人工智障'], #你家bot说话的关键词，也可以把bot名字写这
+    "sleep_time": 60, # 默认60秒，临时会话自动结束
+    "DEFAULT_AI_CHANCE": 0, # 群AI的默认概率
+    "Keywords": ['bot','BOT','Bot','机器人','人工智障'], # 你家bot说话的关键词，不须写bot名字
     "BLACK_WORD": [
         '报刀','状态','删刀','撤销','启用','禁用','预约','催刀','尾刀','建会',
         '查看公会','入会','查看成员', '成员查看', '查询成员', '成员查询','退会',
